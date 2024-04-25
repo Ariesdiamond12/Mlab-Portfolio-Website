96f6721b7bdfd2008074008a4c48b828aef92dbe
Precious <92827167+Ariesdiamond12@users.noreply.github.com>
2024-04-25 10:17:40 +0200

    Added JavaScript Files
    * Added index.js, about.js

---
+++ about.js
@@ -0,0 +1,15 @@
+function personalInformation(){
+
+    const educationBackground = ['2020 - 2022 Bachelor of IT in Business Systems', '2019 - 2019 Higher Certificate in IT in Support Systems', '2018 - Matric Certificate'];
+    console.log(educationBackground);
+    const skills  = ['HTML', 'CSS', 'Basic JavaScript', 'ComptTIA A+', 'End User Computing', 'Help Desk Support', 'Problem Solving'];
+    console.log(skills);
+    const project = ['Project Lead Developer'];
+    console.log(project);
+    let projectDescription = "I was the project lead developer representing ML Data Systems at the AgritourZA 2021 Cohort event where we were tasked with developing a business plan/idea that can change the tourism/agricultural sector in Limpopo";
+    console.log(projectDescription);
+}
+
+personalInformation();
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+function userPersonalDetails(){
+    const firstName  = 'Tumisho Precious Marokane';
+    console.log(firstName);
+    let bio = "Hi, I'm Precious and I'm a programmer. I would describe myself as an ambitious person, if I want something I go for it and I'm a hard worker.";
+    console.log(bio);
+    let phoneNumber = ['079 102 5806/ 081 738 3789'];
+    console.log(phoneNumber);
+    let email = "preciousmarokane@hotmail.co.za";
+    console.log(email);
+}
+userPersonalDetails();
+
+
